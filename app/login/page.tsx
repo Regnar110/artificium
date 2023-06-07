3d127af0a050b7d208d6d07d18e78d8d5c06bff6
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-06-07 17:22:29 +0200

    Login component: Added login button with styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -26,11 +26,12 @@ export default function Home() {
   })
   return (
     <main className='login_page bg-[#131619] grid grid-cols-2 w-full'>
-      <div className='login_section relative grid grid-rows-3 w-full'>
-        <div className='artifictium_logo relative top-5 left-5'>
+      <div className='login_section relative flex flex-col justify-center items-center  w-full'>
+        <div className='artifictium_logo absolute w-fit h-fit top-5 left-5'>
           <Image src={artificium_logo} alt="artificium_logo"/>
         </div>
-        <div className='login_block grid grid-row-6 col-span-2 justify-self-center gap-10'>
+
+        <div className='login_block  grid grid-row-6 row-span-2 justify-self-center place-content-center gap-10'>
           <div className='login_headers flex flex-col gap-y-4'>
             <span className='font-plus_jakarta_sans text-[36px] font-extralight text-white'>Let's get <span className='font-plus_jakarta_sans text-[36px] font-bold text-transparent bg-clip-text gradient_dayblue_blue_green500'>creative!</span></span>
             <span className='text-[#9B9C9E] font-plus_jakarta_sans text-[18px] font-medium'>Log in to Artificium to start creating magic.</span>  
@@ -52,18 +53,33 @@ export default function Home() {
               <div className='line h-[1px] w-full bg-[#363A3D]'></div>
             </div>
           </div> 
-          <form className='login_form flex flex-col gap-y-5'>
-            <ThemeProvider theme={theme}>
-              <TextField InputLabelProps={{ style: {color:"#9B9C9E"}}} sx={{input: {color:"#fff", background:"#363A3D", borderRadius:"10px"}}} color="primary" id="outlined-basic" label="E-mail" variant="outlined" />
-              <TextField InputLabelProps={{ style: {color:"#9B9C9E"}}} sx={{input: {color:"#fff", background:"#363A3D", borderRadius:"10px"}}} color="primary" id="outlined-basic" label="Password" variant="outlined" />              
-            </ThemeProvider>
+          <form className='login_form flex flex-col gap-y-10'>
+            <div className='login_text_fields flex flex-col gap-y-5'>
+              <ThemeProvider theme={theme}>
+                <TextField InputLabelProps={{ style: {color:"#9B9C9E"}}} sx={{input: {color:"#fff", background:"#363A3D", borderRadius:"10px"}}} color="primary" id="outlined-basic" label="E-mail" variant="outlined" />
+                <TextField InputLabelProps={{ style: {color:"#9B9C9E"}}} sx={{input: {color:"#fff", background:"#363A3D", borderRadius:"10px"}}} color="primary" id="outlined-basic" label="Password" variant="outlined" />              
+              </ThemeProvider>
+            </div>
+            <div>CHECKBOXES</div>
+            <Button
+              className='bg-[#B6F09C] font-plus_jakarta_sans text-[#0C1132] font-bold'
+              sx={{
+                ':hover': {
+                  bgcolor: '#9bf074', // theme.palette.primary.main
+                },
+              }}
+            >
+                Log in
+              </Button>
           </form>   
-          <div className='signup flex gap-3 relative justify-self-start font-semibold text-[#686B6E]'>Don't have an accont?<span className='font-plus_jakarta_sans text-[16px] font-semibold text-transparent bg-clip-text gradient_dayblue_blue_green500'>Sign Up</span></div>    
+          <div className='signup flex gap-3 relative justify-self-center font-semibold text-[#686B6E]'>Don't have an accont?<span className='font-plus_jakarta_sans text-[16px] font-semibold text-transparent bg-clip-text gradient_dayblue_blue_green500'>Sign Up</span></div>    
         </div>
+
       </div>
-      <div className='login_illustration relative w-full justify-self-end float-right'>
+      
+      <div className='login_illustration relative w-full justify-self-end float-right h-fit'>
         <Image
-          className='float-right object-contain h-[100vh] w-fit' 
+          className='float-right object-contain w-fit' 
           src={login_image}
           alt='login_image'
         />
