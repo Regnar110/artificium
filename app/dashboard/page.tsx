26744d3ccad57bf0a93860fa0e2e4b7da5995d15
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-07-25 17:59:29 +0200

    Added socket.io and socket.io-client in order to connect socket on our server to keep conection beetwen server and clieant all the time. It will be needed to implement real time chat.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client' // FOR SOCKET CLIENT TEST
+import { io } from "socket.io-client";
+import React, { useEffect } from 'react'
 import UserPanel from './DashBoardComponents/UserPanel/UserPanel'
 import UserBoardWrapper from './DashBoardComponents/UserBoard/UserBoardWrapper'
 import ChatingWindowsWrapper from './DashBoardComponents/UserBoard/Components/ChattingWindows/ChatingWindowsWrapper'
@@ -8,6 +10,16 @@ import ChatPanel from './DashBoardComponents/ChatPanel/ChatPanel'
 import DashboardPageWrapper from './DashBoardComponents/DashboardPageWrapper/DashboardPageWrapper'
 
 const Dashboard = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:3001/')
+    socket.on('messageReceived', (message) => {
+      console.log("wiadomosc to", message)
+      socket.emit("SIemanooooo","siemka")
+    })
+    return () => {
+      socket.disconnect()
+    }
+  },[])
   return (
     <DashboardPageWrapper>
         <UserPanel/>        
