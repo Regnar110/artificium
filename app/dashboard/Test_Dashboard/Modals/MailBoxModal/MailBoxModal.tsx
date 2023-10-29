a75550be4fd2a25f8776c0cde3d844e1fd7c9095
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-10-30 00:37:09 +0100

    Preparing scrollable mailbox

--- app/dashboard/Test_Dashboard/Modals/MailBoxModal/MailBoxModal.tsx
+++ app/dashboard/Test_Dashboard/Modals/MailBoxModal/MailBoxModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+
 import { ThemeProvider } from '@emotion/react';
-import { createTheme, TextField } from '@mui/material';
+import { Checkbox, createTheme, TextField } from '@mui/material';
 import ModalFooter from '../Components/ModalFooter';
 import GlassModal from '../GlassModal/GlassModal';
 
@@ -31,6 +32,77 @@ const MailBoxModal = ({modalIsOpen, setModal}:Props) => {
               InputLabelProps={{style:{color:"white"}}} 
           />            
       </ThemeProvider>
+      <section className='grid_scroll_mailbox bg-[#0D0F10] h-full rounded-md px-1'>
+        <h5 className='bg-[#0D0F10] text-[#9B9C9E] text-[16px] w-full px-4 py-2'>Mailbox</h5>
+        <article className='scrollable_mailbox_container px-1 py-1 flex flex-col h-[200px] overflow-x-hidden overflow-y-auto scrollbar scrollbar-w-1 scrollbar-thumb-[#0D0F10] scrollbar-track-transparent'>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          <div className='mailbox_mail text-white grid grid-cols-10 hover:bg-[#ffffff13] cursor-pointer px-3 py-3 rounded-md place-content-center place-items-center'>
+            <form >
+              <Checkbox size='small'/>
+            </form>
+            <div className='col-span-2 text-[#B6F09C] font-medium text-[16px] '>Mateusz W</div>
+            <div className="col-span-7 text-ellipsis whitespace-nowrap overflow-hidden pr-5 text-[#9B9C9E] hover:text-[#ffffff] transition-all duration-300">TOPIC z częścią wiadomosćiadsasdasdasdasdasdasdasdasdsdasdas</div>
+          </div>
+          
+        </article>
+
+      </section>
       <ModalFooter/>
     </GlassModal>
   )
